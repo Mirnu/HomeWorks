a821d38a966a23452ae987a2fe06f93b6d827132
Mernya <104453992+Mirnu@users.noreply.github.com>
2024-12-04 14:47:53 +0300

    Create game.js

---
+++ game.js
@@ -0,0 +1,28 @@
+function generateSecretNumber() {
+    return Math.floor(Math.random() * 100) + 1;
+}
+
+function playGame() {
+    const secretNumber = generateSecretNumber();
+    let low = 1;
+    let high = 100;
+    let guess;
+
+    console.log(`Компьютер 1 загадал число: ${secretNumber}.`);
+
+    while (true) {
+        guess = Math.floor((low + high) / 2);
+        console.log(`Компьютер 2: Пробую число ${guess}.`);
+
+        if (guess < secretNumber) {
+            console.log(`Компьютер 1: Больше.`);
+            low = guess + 1; 
+        } else if (guess > secretNumber) {
+            console.log(`Компьютер 1: Меньше.`);
+            high = guess - 1; 
+        } else {
+            console.log(`Компьютер 1: Угадал!`);
+            break; 
+        }
+    }
+}
